2a87678ca5342f2c335ff696cddb72458bf6c458
SantiRosso <santiagorosso@live.com.ar>
2023-06-13 00:28:27 -0300

    imagen agregada al about

--- client/src/components/Views/Home/Home.jsx
+++ client/src/components/Views/Home/Home.jsx
@@ -4,12 +4,18 @@ import NavBar from "../../NavBar/NavBar";
 import Projects from "../../Projects/Projects";
 //styles
 import s from "./Home.module.css";
+//images
+import programador from "./programador.png";
 
 const Home = () => {
     return(
         <div className={s.container}>
             <NavBar/>
-            <About/>
+            <div className={s.about}>
+                <img className={s.img} src={programador} alt="Imagen de programador" />
+                <About/>
+            </div>
+            
             {/* <Technologies/> */}
             <Projects/>
             {/* <Contact/> */}
