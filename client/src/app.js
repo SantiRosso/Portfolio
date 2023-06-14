11cce93988e401c81b1cf8682f4ff3e0e65ab90d
SantiRosso <santiagorosso@live.com.ar>
2023-06-14 01:31:38 -0300

    ProjectDetail styles

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./app.css";
 //components
-import LandingPage from "./components/Views/LandingPage/LandingPage";
+// import LandingPage from "./components/Views/LandingPage/LandingPage";
 import Home from "./components/Views/Home/Home";
 import ProjectDetail from "./components/Views/ProjectDetail/ProjectDetail";
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
+        {/* <Route exact path="/" element={<LandingPage />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/projectDetail/:id" element={<ProjectDetail />} />
       </Routes>
     </div>
--- client/src/components/Views/ProjectDetail/ProjectDetail.jsx
+++ client/src/components/Views/ProjectDetail/ProjectDetail.jsx
@@ -1,5 +1,9 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+//components
+import NavBar from "../../NavBar/NavBar";
+//style
+import s from "./ProjectDetail.module.css";
 
 const ProjectDetail = () => {
 
@@ -7,13 +11,12 @@ const ProjectDetail = () => {
     const projects = useSelector((state) => state.projects);
 
     const project = projects?.filter((e) => e.id == id)
-    console.log("project", project[0])
-    console.log(projects)
-    console.log(id)
 
     return(
-        <div>
-            Detalle del proyecto
+        <div className={s.container}>
+            <NavBar/>
+            <h1>{project[0].name}</h1>
+            <img src={project[0].img} alt={project[0].name} />
         </div>
     )
 }
