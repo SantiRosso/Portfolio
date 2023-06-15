1e5699ba04638ddc4feb4c16a22e6cb51b15784d
SantiRosso <santiagorosso@live.com.ar>
2023-06-15 20:21:40 -0300

    window.scrollTo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,18 +2,25 @@ import Button from "../Button/Button";
 import s from "./NavBar.module.css";
 
 const NavBar = () => {
-    // window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+    
+    const handleClick = (id) => {
+        console.log(id)
+        const element = document.getElementById(id);
+        const position = element.getBoundingClientRect();
+        window.scrollTo({ top: position.top, left: position.left, behavior: 'smooth' })
+    }
+
     return(
         <div className={s.container}>
             <div className={s.title}>
                 <span>Santiago Rosso</span>
             </div>
             <div className={s.content}>
-                <a href="#about" className={s.link}><span className={s.span}>Sobre mi</span></a>
-                <a href="#projects" className={s.link}><span className={s.span}>Proyectos</span></a>
-                <a href="#technologies" className={s.link}><span className={s.span}>Tecnologías</span></a>
-                <a href="#education" className={s.link}><span className={s.span}>Educación</span></a>
-                <a href="#contact" className={s.link}><span className={s.span}>Contacto</span></a>
+                <a /* href="#about" */ className={s.link} onClick={()=>handleClick("about")}><span className={s.span}>Sobre mi</span></a>
+                <a /* href="#projects" */ className={s.link} onClick={()=>handleClick("projects")}><span className={s.span}>Proyectos</span></a>
+                <a /* href="#technologies" */ className={s.link} onClick={()=>handleClick("technologies")}><span className={s.span}>Tecnologías</span></a>
+                <a /* href="#education" */ className={s.link} onClick={()=>handleClick("education")}><span className={s.span}>Educación</span></a>
+                <a /* href="#contact" */ className={s.link} onClick={()=>handleClick("contact")}><span className={s.span}>Contacto</span></a>
             </div>
         </div>
     )
