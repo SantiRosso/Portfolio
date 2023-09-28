8ef75909354f9cc7c2790a9b64562b56b6aa5fd7
SantiRosso <santiagorosso@live.com.ar>
2023-09-27 21:13:31 -0300

    technologies.js refactor

---
+++ client/src/assets/technologies.js
@@ -0,0 +1,121 @@
+import cloudinary from "./cloudinarypng.png";
+import emailJs from "./emailJS.png";
+import nodemailer from "./nodemailer.png";
+import chakra from "./chakra.png";
+import typeorm from "./typeorm.png";
+
+export const front = [
+  {
+    name: "HTML",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    name: "Javascript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "Typescript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "React",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
+  },
+  {
+    name: "Redux",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+  },
+  {
+    name: "Next.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+  },
+  {
+    name: "Angular",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+  },
+  {
+    name: "Bootstrap",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+  },
+  {
+    name: "Tailwind",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+  },
+  { name: "Chakra UI", img: chakra },
+];
+// {name: "MUI", img: }
+// {name: "React Native", img:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", style={{backgroundColor: "#1E1E1E", borderRadius: "40%"}}
+// {name: Flutter, img:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg"}
+
+export const back = [
+  {
+    name: "Node.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "Express",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+  },
+  {
+    name: "PostgreSQL",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg",
+  },
+  {
+    name: "Sequelize",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg",
+  },
+  {
+    name: "MongoDB/Mongoose",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "NestJS",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-plain.svg",
+  },
+  {
+    name: "MySQL",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+  },
+  { name: "TypeORM", img: typeorm },
+];
+//{name: GoogleCloud, img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg"}
+
+export const otras = [
+  {
+    name: "NPM",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
+  },
+  {
+    name: "Git",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+  },
+  {
+    name: "Github",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+  },
+  {
+    name: "Firebase",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+  },
+  { name: "Cloudinary", img: cloudinary },
+  { name: "Chart.js", img: "https://www.chartjs.org/media/logo-title.svg" },
+  {
+    name: "Postman",
+    img: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+  },
+  { name: "EmailJs", img: emailJs },
+  { name: "Nodemailer", img: nodemailer },
+  {
+    name: "Trello",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg",
+  },
+];
+//{name: "Insomnia", img: }
+//{name: "Babel", img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg"}
+//{name: "Webpack", img: ""https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg""}
+//{name: "Andriod Studio", img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg"}
+//{name: "Docker", img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"}
